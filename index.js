8e5cda15890958d7d1ca96a83e56555081c477e7
Davis <davisguybaker@gmail.com>
2024-03-28 23:43:05 -0400

    got etch-a-sketch function working, added event listeners, all the good stuff

--- index.js
+++ index.js
@@ -5,6 +5,25 @@ for(let i = 0; i < num; i++){
     let currentDiv = document.createElement("div");
     currentDiv.setAttribute("id",`row${i}`);
     currentDiv.setAttribute("class","row");
-    currentDiv.textContent = `this is row${i}`;
     containerDiv.appendChild(currentDiv);
+}
+
+for(let i = 0; i < num; i++){
+    for(let j = 0; j < num; j++){
+        let currentDiv = document.createElement("div");
+        currentDiv.setAttribute("id",`b${j}r${i}`);
+        currentDiv.setAttribute("class","square");
+        let currentDivId = currentDiv.id;
+        currentDiv.addEventListener("mouseenter", function() {
+            changeBox(currentDivId);
+            console.log(`fired event listener with ${currentDivId}`);
+        })
+        let rowDiv = document.querySelector(`#row${i}`);
+        rowDiv.appendChild(currentDiv);
+    }
+}
+
+function changeBox(divId){
+    let divToChange = document.querySelector(`#${divId}`);
+    divToChange.classList.add("drawn");
 }
