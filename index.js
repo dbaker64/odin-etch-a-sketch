cfb9cd232a9fb91b7df952c20182388b1b63b5b8
Davis <davisguybaker@gmail.com>
2024-03-29 08:51:39 -0400

    added reset button to clear easel once you're done drawing

--- index.js
+++ index.js
@@ -1,25 +1,46 @@
-let num = prompt("How many rows/columns would you like?");
+let num;
 let containerDiv = document.querySelector("#container");
+let resetButton = document.querySelector("#reset");
+resetButton.addEventListener("click", function() {
+    reset();
+})
 
-for(let i = 0; i < num; i++){
-    let currentDiv = document.createElement("div");
-    currentDiv.setAttribute("id",`row${i}`);
-    currentDiv.setAttribute("class","row");
-    containerDiv.appendChild(currentDiv);
+function getNum() {
+    num = prompt("Please enter the number of rows/columns you want");
+    if(num > 100){
+        answer = prompt("That's too many rows, are you sure you want that many? Y/N");
+        answer = answer.toLowerCase();
+        if(answer === "n"){
+            num = prompt("Please enter the number of rows/columns you want");
+        }
+    }
+    createRows();
 }
 
-for(let i = 0; i < num; i++){
-    for(let j = 0; j < num; j++){
+function createRows() {
+    for(let i = 0; i < num; i++){
         let currentDiv = document.createElement("div");
-        currentDiv.setAttribute("id",`b${j}r${i}`);
-        currentDiv.setAttribute("class","square");
-        let currentDivId = currentDiv.id;
-        currentDiv.addEventListener("mouseenter", function() {
-            changeBox(currentDivId);
-            console.log(`fired event listener with ${currentDivId}`);
-        })
-        let rowDiv = document.querySelector(`#row${i}`);
-        rowDiv.appendChild(currentDiv);
+        currentDiv.setAttribute("id",`row${i}`);
+        currentDiv.setAttribute("class","row");
+        containerDiv.appendChild(currentDiv);
+    }
+    createBoxes();
+}
+
+function createBoxes() {
+    for(let i = 0; i < num; i++){
+        for(let j = 0; j < num; j++){
+            let currentDiv = document.createElement("div");
+            currentDiv.setAttribute("id",`b${j}r${i}`);
+            currentDiv.setAttribute("class","square");
+            let currentDivId = currentDiv.id;
+            currentDiv.addEventListener("mouseenter", function() {
+                changeBox(currentDivId);
+                console.log(`fired event listener with ${currentDivId}`);
+            })
+            let rowDiv = document.querySelector(`#row${i}`);
+            rowDiv.appendChild(currentDiv);
+        }
     }
 }
 
@@ -27,3 +48,19 @@ function changeBox(divId){
     let divToChange = document.querySelector(`#${divId}`);
     divToChange.classList.add("drawn");
 }
+
+function reset() {
+    let gridDivs = document.querySelectorAll(".square");
+    console.log(gridDivs);
+    gridDivs.forEach(element => {
+        element.remove();
+    })
+    let rowDivs = document.querySelectorAll(".row");
+    rowDivs.forEach(element => {
+        element.remove();
+    })
+    getNum();
+}
+
+//starting program
+getNum();
